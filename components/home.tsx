3db4cadd3b62be79e6798bceeda0236e255714ab
MatthewKokin <135328101+MatthewKokin@users.noreply.github.com>
2023-11-20 22:40:59 +0000

    commit initial files

--- components/home.tsx
+++ components/home.tsx
@@ -4,16 +4,22 @@ import Image from 'next/image';
 export function Home() {
   return (
     <div>
-        <h1 className='mission'>Nuclear Waste Management Starts Here: UK path to a more sustainable HLW management</h1>
-        <button>See Economic Model</button>
+        <div className='mission'>
+            <h1>Nuclear Waste Management</h1>
+            <h1>Starts Here: UK path to a</h1>
+            <h1>more sustainable HLW</h1>
+            <h1>management</h1>
+        </div>
+        
         <Image
-                    src="/infographic2.svg"
-                    alt="Logo"
-                    height={1000}
-                    width={1000}
-                    className="m-0"
-                />
+            src="/infographic.svg"
+            alt="Logo"
+            height={1000}
+            width={1000}
+            className="m-0 img"
+            />
         <div>
+        <button>See Economic Model</button>
 
         </div>
     </div>
