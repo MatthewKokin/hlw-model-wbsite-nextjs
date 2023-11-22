54bdc4399f083bc9eb9206f083fa47030039f799
MatthewKokin <135328101+MatthewKokin@users.noreply.github.com>
2023-11-22 03:08:51 +0000

    fix: home page done

--- app/(marketing)/_components/about.tsx
+++ app/(marketing)/_components/about.tsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import Image from 'next/image'
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
+  
 
 function About() {
   return (
-    <div>About</div>
+    <div>
+        <div>
+        <Card>
+            <CardHeader>
+                <CardTitle>Matthew K</CardTitle>
+                <CardDescription>CFO</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <Image
+                src="/icon.jpeg"
+                alt="Logo"
+                height={440}
+                width={440}
+                className=""
+                />
+                <p>Card Content</p>
+            </CardContent>
+            <CardFooter>
+                <p>Card Footer</p>
+            </CardFooter>
+        </Card>
+        </div>
+    </div>
   )
 }
 
--- app/(marketing)/_components/home.tsx
+++ app/(marketing)/_components/home.tsx
@@ -10,20 +10,20 @@ export function Home() {
             <h1>Starts Here: UK path to a</h1>
             <h1>more sustainable HLW</h1>
             <h1>management</h1>
+          <div className='container-img'>
+          <Image
+              src="/infographic.svg"
+              alt="Logo"
+              height={440}
+              width={440}
+              className="img"
+              />
+          </div>
+          <div className='container-button'>
+            <button>See Economic Model</button>
+          </div>
         </div>
         
-        <div className='container-img'>
-        <Image
-            src="/infographic.svg"
-            alt="Logo"
-            height={440}
-            width={440}
-            className="img"
-            />
-      </div>
-        </div>
-        <div className='container-button'>
-          <button>See Economic Model</button>
         </div>
     </div>
   )
