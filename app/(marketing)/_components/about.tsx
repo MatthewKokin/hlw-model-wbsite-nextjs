570e52327feb9e437154a8f84c8c73a986e1b978
MatthewKokin <135328101+MatthewKokin@users.noreply.github.com>
2023-11-22 21:54:54 +0000

    feat: about section done

--- app/(marketing)/_components/about.tsx
+++ app/(marketing)/_components/about.tsx
@@ -8,123 +8,44 @@ import {
     CardFooter,
     CardHeader,
     CardTitle,
-  } from "@/components/ui/card"
-  
+} from "@/components/ui/card"
 
 function About() {
-  return (
-    <div className='about-container'>
-        <h1>Who we are?</h1>
-        <div className='cards'>
-        <div className='card'>
-        <Card>
-            <CardHeader>
-                <CardTitle>Will R</CardTitle>
-                <CardDescription>CSO</CardDescription>
-            </CardHeader>
-            <CardContent>
-                <Image
-                src="/icon.jpeg"
-                alt="Logo"
-                height={100}
-                width={100}
-                className=""
-                />
-                <p>Card Content</p>
-            </CardContent>
-            <CardFooter>
-                <p>Card Footer</p>
-            </CardFooter>
-        </Card>
+    // Define your data array
+    const teamMembers = [
+        { name: "Matthew", title: "Financial Adviser​", icon: "/mat_icon.png"},
+        { name: "Josie", title: "Environmental Adviser​", icon: "/josie_icon.png"},
+        { name: "Will", title: "Scientific Advisor​​", icon: "/will_icon.png"},
+        { name: "Chloe", title: "Scientific Advisor​​", icon: "/chloe_icon.png"},
+        { name: "Will", title: "Strategy Advisor​​", icon: "/hymaria_icon.png"},
+    ];
+
+    return (
+        <div className='about-container'>
+            <h1>Who we are?</h1>
+            <div className='cards'>
+                {teamMembers.map(member => (
+                    <div className='card' key={member.name}>
+                        <Card>
+                            <CardHeader>
+                                <CardTitle>{member.name}</CardTitle>
+                            </CardHeader>
+                            <CardContent>
+                                <Image
+                                    src={member.icon}
+                                    alt="Logo"
+                                    height={120}
+                                    width={120}
+                                    className="img"
+                                />
+                                <p>{member.title}</p>
+                            </CardContent>
+                        </Card>
+                    </div>
+                ))}
+            </div>
         </div>
-        <div className='card'>
-        <Card>
-            <CardHeader>
-                <CardTitle>Will R</CardTitle>
-                <CardDescription>CSO</CardDescription>
-            </CardHeader>
-            <CardContent>
-                <Image
-                src="/icon.jpeg"
-                alt="Logo"
-                height={100}
-                width={100}
-                className=""
-                />
-                <p>Card Content</p>
-            </CardContent>
-            <CardFooter>
-                <p>Card Footer</p>
-            </CardFooter>
-        </Card>
-        </div>
-        <div className='card'>
-        <Card>
-            <CardHeader>
-                <CardTitle>Will R</CardTitle>
-                <CardDescription>CSO</CardDescription>
-            </CardHeader>
-            <CardContent>
-                <Image
-                src="/icon.jpeg"
-                alt="Logo"
-                height={100}
-                width={100}
-                className=""
-                />
-                <p>Card Content</p>
-            </CardContent>
-            <CardFooter>
-                <p>Card Footer</p>
-            </CardFooter>
-        </Card>
-        </div>
-        <div className='card'>
-        <Card>
-            <CardHeader>
-                <CardTitle>Will R</CardTitle>
-                <CardDescription>CSO</CardDescription>
-            </CardHeader>
-            <CardContent>
-                <Image
-                src="/icon.jpeg"
-                alt="Logo"
-                height={100}
-                width={100}
-                className=""
-                />
-                <p>Card Content</p>
-            </CardContent>
-            <CardFooter>
-                <p>Card Footer</p>
-            </CardFooter>
-        </Card>
-        </div>
-        <div className='card'>
-        <Card>
-            <CardHeader>
-                <CardTitle>Will R</CardTitle>
-                <CardDescription>CSO</CardDescription>
-            </CardHeader>
-            <CardContent>
-                <Image
-                src="/icon.jpeg"
-                alt="Logo"
-                height={100}
-                width={100}
-                className=""
-                />
-                <p>Card Content</p>
-            </CardContent>
-            <CardFooter>
-                <p>Card Footer</p>
-            </CardFooter>
-        </Card>
-        </div>
-        </div>
-        
-    </div>
-  )
+    )
 }
 
-export default About
+export default About;
